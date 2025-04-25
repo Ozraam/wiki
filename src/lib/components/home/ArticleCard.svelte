fb27fbf58879c48a6479ece060390bdc510c8751
Tristan <ardouin.t@france-elevateur.fr>
2025-04-25 16:37:21 +0200

    feat: Add article listing and detail components with associated data structure

---
+++ src/lib/components/home/ArticleCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Article } from "$lib/type";
+
+  const { article }: { article: Article } = $props();
+</script>
+
+<a
+  href="/article/{article.id}"
+  class="flex flex-col p-4 rounded-xl shadow-md ring ring-gray-200"
+>
+  <h3 class="text-lg font-bold mt-2 ">{article.title}</h3>
+  <small>
+    <span class="text-gray-500">{article.author}</span> -
+    <span class="text-gray-500">{new Date(article.date).toLocaleDateString()}</span>
+  </small>
+  <p class="text-gray-600 text-ellipsis">{article.description}</p>
+</a>
