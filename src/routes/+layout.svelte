bee9cb2fabc7fc1f3f42dead420f3625a3eda108
Tristan <ardouin.t@france-elevateur.fr>
2025-04-25 12:07:35 +0200

    Add PWA support with vite-plugin-pwa and update configurations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { pwaInfo } from 'virtual:pwa-info';
 	
 	let { children } = $props();
+	let webManifestLink = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : null);
 </script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
 
 {@render children()}
